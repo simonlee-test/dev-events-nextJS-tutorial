d8da52fe4128bb060e1c0ca1e0cb5354ab4adc8a
simonlee-test <simonlee190301@gmail.com>
2026-08-27 18:09:36 +0800

    feat: setup light rays and homepage background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import LightRays from '@/components/LightRays';
 
-const schibstedGrotesk= Schibsted_Grotesk({
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
+
+const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
   subsets: ["latin"],
 });
@@ -21,9 +25,28 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen h-full antialiased`}
+      className={cn("min-h-screen", "h-full", "antialiased", schibstedGrotesk.variable, martianMono.variable, "font-sans", geist.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+
+
+        <div className="absolute inset-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0.01}
+
+          />
+        </div>
+        <main>
+          {children}
+        </main></body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Welcome to Next.js 16! </div>
-  )
+    <section><h1 className='text-center'>The Hub for Every Dev <br />Event You Can't Miss</h1>
+      <p className='mt-5 text-center'>Hackathon, Meetups, and Conferences, All in One Place</p></section>)
 }
 
 export default Page
