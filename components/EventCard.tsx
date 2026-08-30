d5d26b764b032bcdcda0e27c81aeda16d2d93483
simonlee-test <simonlee190301@gmail.com>
2026-08-30 21:26:47 +0800

    build: setup posthog

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import { EventItem } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
-
+import posthog from "posthog-js";
 
 const EventCard = ({title, image, slug, location, date, time}: EventItem) => {
+  const handleEventOpen = () => {
+    posthog.capture("event_detail_opened", { event_slug: slug });
+  };
+
   return (
-    <Link href={`/events/${slug}`} id='event-card'>
+    <Link href={`/events/${slug}`} id='event-card' onClick={handleEventOpen}>
       <Image src={image} alt={title} width={410} height={300} className="poster"/>
       <div className="flex flex-row gap-2">
         <Image src={'/icons/pin.svg'} alt="location" width={14} height={14}/>
--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import Image from "next/image"
-
-//Whenever we have functions like onClick, it must be turned into a client component.
+import posthog from "posthog-js"
 
 const ExploreBtn = () => {
+  const handleExplore = () => {
+    posthog.capture("events_explored")
+  }
+
   return (
-    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={()=>console.log("click")}>
+    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={handleExplore}>
       <a href="#events">Explore Events</a>
       <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
     </button>
